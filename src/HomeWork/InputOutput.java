b95960c106dc56bae055000b01eebd065dc0854b
JolyJesus <44112418+JolyJesus@users.noreply.github.com>
2020-03-20 23:29:42 +0200

    Soft

---
+++ src/HomeWork/InputOutput.java
@@ -0,0 +1,19 @@
+package HomeWork;
+
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main (String[] args){
+        String name, city;
+
+        System.out.println("What is your name: ");
+        Scanner n = new Scanner(System.in);
+        name = n.nextLine();
+
+        System.out.println("Where are you live, " + name);
+        Scanner c = new Scanner(System.in);
+        city = c.nextLine();
+
+        System.out.println("My name is " + name + ", and i live in " + city);
+    }
+}
